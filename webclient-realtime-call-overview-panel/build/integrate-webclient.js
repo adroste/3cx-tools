60e03785953ed22410bb68cbbf11b48793639e8f
Alexander Droste <mail@adroste.com>
2021-12-19 04:17:27 +0100

    fix: integrate-webclient.js match regex bug

--- webclient-realtime-call-overview-panel/build/integrate-webclient.js
+++ webclient-realtime-call-overview-panel/build/integrate-webclient.js
@@ -2,8 +2,8 @@
   fetch('/webclient/tcx-tools-panel-app/index.html')
     .then((res) => res.text())
     .then((indexHtml) => {
-      const cssRe = /href="(\/webclient\/tcx-tools-panel-app\/.+\.css)"/;
-      const scriptRe = /src="(\/webclient\/tcx-tools-panel-app\/.+\.js)"/;
+      const cssRe = /href="(\/webclient\/tcx-tools-panel-app\/static\/css\/[a-zA-Z0-9.]+\.css)"/;
+      const scriptRe = /src="(\/webclient\/tcx-tools-panel-app\/static\/js\/[a-zA-Z0-9.]+\.js)"/;
       const cssHref = cssRe.exec(indexHtml);
       const scriptSrc = scriptRe.exec(indexHtml);
 
